d4f1b988c2ce2ba7be431cb33afe8f0d3e28efa1
daame <barradeespacoe@gmail.com>
2025-05-31 15:53:47 -0300

    model usuarios criado

---
+++ server/src/models/Usuarios.js
@@ -0,0 +1,71 @@
+const { DataTypes, Model } = require("sequelize");
+const sequelize = require("../config/database");
+const bcrypt = require("bcrypt");
+
+const TiposUsuario = {
+  ADM_ESPACO: "Adm_espaco",
+  ADM_FESTA: "Adm_festa",
+  CONVIDADO: "Convidado",
+};
+
+class Usuario extends Model {
+  async compararSenha(senha) {
+    return bcrypt.compare(senha, this.senha);
+  }
+}
+
+Usuario.init({
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+
+  nome: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+  },
+
+  senha: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+
+  tipo: {
+    type: DataTypes.ENUM(
+      TiposUsuario.ADM_ESPACO,
+      TiposUsuario.ADM_FESTA,
+      TiposUsuario.CONVIDADO
+    ),
+    allowNull: false,
+    defaultValue: TiposUsuario.CONVIDADO,
+  },
+
+  sequelize,
+  modelName: "Usuario",
+  tableName: "usuarios",
+  hooks: {
+    beforeCreate: async (usuario) => {
+      if (usuario.changed("senha")) {
+        const salt = await bcrypt.genSalt(10);
+        usuario.senha = await bcrypt.hash(usuario.senha, salt);
+      }
+    },
+    beforeUpdate: async (usuario) => {
+      if (usuario.changed("senha")) {
+        const salt = await bcrypt.genSalt(10);
+        usuario.senha = await bcrypt.hash(usuario.senha, salt);
+      }
+    },
+  },
+});
+
+Usuario.TIPOS_USUARIO = TIPOS_USUARIO; // Exportando os tipos de usuário
+
+module.exports = Usuario;
