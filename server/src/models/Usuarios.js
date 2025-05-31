76179e6caba460e1cff9e66643fbe061847704c7
daame <barradeespacoe@gmail.com>
2025-05-31 16:58:00 -0300

    Ajustes no model usuario para ficar de acordo com a tabela mysql + criação do controller para login e registro de usuarios pelos tipos

--- server/src/models/Usuarios.js
+++ server/src/models/Usuarios.js
@@ -2,7 +2,7 @@ const { DataTypes, Model } = require("sequelize");
 const sequelize = require("../config/database");
 const bcrypt = require("bcrypt");
 
-const TiposUsuario = {
+const TIPOS_USUARIO = {
   ADM_ESPACO: "Adm_espaco",
   ADM_FESTA: "Adm_festa",
   CONVIDADO: "Convidado",
@@ -14,62 +14,70 @@ class Usuario extends Model {
   }
 }
 
-Usuario.init({
-  id: {
-    type: DataTypes.INTEGER,
-    primaryKey: true,
-    autoIncrement: true,
-  },
-
-  telefone: {
-    type: DataTypes.STRING,
-    allowNull: true,
-  },
-
-  nome: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-
-  email: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    unique: true,
-  },
-
-  senha: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-
-  tipo: {
-    type: DataTypes.ENUM(
-      TiposUsuario.ADM_ESPACO,
-      TiposUsuario.ADM_FESTA,
-      TiposUsuario.CONVIDADO
-    ),
-    allowNull: false,
-    defaultValue: TiposUsuario.CONVIDADO,
-  },
-
-  sequelize,
-  modelName: "Usuario",
-  tableName: "usuarios",
-  hooks: {
-    beforeCreate: async (usuario) => {
-      if (usuario.changed("senha")) {
-        const salt = await bcrypt.genSalt(10);
-        usuario.senha = await bcrypt.hash(usuario.senha, salt);
-      }
+Usuario.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
     },
-    beforeUpdate: async (usuario) => {
-      if (usuario.changed("senha")) {
-        const salt = await bcrypt.genSalt(10);
-        usuario.senha = await bcrypt.hash(usuario.senha, salt);
-      }
+
+    telefone: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+
+    nome: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: {
+          msg: "O email deve ser um endereço de email válido.",
+        },
+      },
+    },
+
+    senha: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+
+    tipoUsuario: {
+      type: DataTypes.ENUM(
+        TIPOS_USUARIO.ADM_ESPACO,
+        TIPOS_USUARIO.ADM_FESTA,
+        TIPOS_USUARIO.CONVIDADO
+      ),
+      allowNull: false,
+      defaultValue: TiposUsuario.CONVIDADO,
     },
   },
-});
+  {
+    sequelize,
+    modelName: "Usuario",
+    tableName: "usuarios",
+    hooks: {
+      beforeCreate: async (usuario) => {
+        if (usuario.changed("senha")) {
+          const salt = await bcrypt.genSalt(10);
+          usuario.senha = await bcrypt.hash(usuario.senha, salt);
+        }
+      },
+      beforeUpdate: async (usuario) => {
+        if (usuario.changed("senha")) {
+          const salt = await bcrypt.genSalt(10);
+          usuario.senha = await bcrypt.hash(usuario.senha, salt);
+        }
+      },
+    },
+  }
+);
 
 Usuario.TIPOS_USUARIO = TIPOS_USUARIO; // Exportando os tipos de usuário
 
